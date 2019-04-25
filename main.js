faf0f7fd8fbd314411f3e6114e817c3339c1bfd0
Abe <abezukor@gmail.com>
2019-04-25 11:33:56 -0700

    Added a test website. Its broken.

--- main.js
+++ main.js
@@ -1,54 +1,85 @@
-var majorearningdata = {
-    'education': 40050,
-    'mathematics': 50340,
-    'business_marketing': 50200,
-    'communications_technology': 45260,
-    'language': 40020,
-    'visual_performing': 36270,
-    'engineering_technology': 65480,
-    'parks_recreation_fitness': 40080,
-    'agriculture': 45330,
-    'security_law_enforcement': 40900,
-    'computer': 65440,
-    'precision_production': 65480,
-    'humanities': 40020,
-    'library': 40280,
-    'psychology': 40100,
-    'social_science': 40030,
-    'legal': 50330,
-    'english': 40280,
-    'construction': 48270,
-    'military': 48270,
-    'communication': 45260,
-    'public_administration_social_service': 36200,
-    'architecture': 48270,
-    'ethnic_cultural_gender': 40030,
-    'resources': 48270,
-    'health': 56350,
-    'engineering': 65480,
-    'history': 43430,
-    'theology_religious_vocation': 40050,
-    'transportation': 48270,
-    'physical_science': 49110,
-    'science_technology': 60100,
-    'biological': 45330,
-    'family_consumer_science': 36200,
-    'philosophy_religious': 40050,
-    'personal_culinary': 48270,
-    'multidiscipline': 43170,
-    'mechanic_repair_technology': 71860
-};
+function loadFile(filePath) {
+  var result = null;
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET", filePath, false);
+  xmlhttp.send();
+  if (xmlhttp.status==200) {
+    result = xmlhttp.responseText;
+  }
+  return result;
+}
 
-$( function() {
-  $.ajax({
-      url:'collegelist.txt',
-      success: function (data){
-        var collegelist = data.split();
-      }
-    });
-    
-    $( "#tags" ).autocomplete({
-      source: collegelist
-    });
-  
+function getIncome(){
+  majorearningdata = JSON.parse(loadFile("majordata.json"));
+
+  var collegename = document.getElementById("collegeSearch").value;
+  var testmajor = $("#majors :selected").val();
+
+  var url = "https://api.data.gov/ed/collegescorecard/v1/schools?school.name={0}&api_key=pjocLbVezV0ADpMYlUBYtNJYt4ObWiXtFiGgvnDr";
+
+  var webdata = JSON.parse(loadFile("https://api.data.gov/ed/collegescorecard/v1/schools?school.name=" + 
+  (collegename.replace(/ /g,"%20")) + "&api_key=pjocLbVezV0ADpMYlUBYtNJYt4ObWiXtFiGgvnDr"))['results'][0]['latest'];
+
+  var earnings = webdata['earnings'];
+  var majorratios = webdata['academics']['program_percentage'];
+
+  var completion = webdata['completion'];
+  var gradrate = completion['completion_rate_4yr_200nt'];
+
+  var idealavg = 0;
+  for(var major in majorratios){
+    try{idealavg += majorratios[major]*majorearningdata[major];}
+    catch(err){}
+  }
+
+  var avgdata= {}
+
+  for(var year in earnings){
+    try{if(earnings[year].length<2){continue;}}
+    catch(err){continue;}
+
+    try{avgdata[year] = parseInt(earnings[year]['mean_earnings']);} // Look for a mean earnings entry
+    catch(err){avgdata[year] = 0;}
+    if (avgdata[year]>0){continue;}
+
+    try{avgdata[year] = parseInt(earnings[year]['mean_earnings']['middle_tercile']);} // Look for a mean earnings/middle tercile entry
+    catch(err){avgdata[year]=0;}
+    if (avgdata[year]>0){continue;}
+
+    try{avgdata[year] = parseInt(earnings[year]['median']);} // Look for a mean earnings/middle tercile entry
+    catch(err){avgdata[year]=0;}
+    if (avgdata[year]>0){continue;}
+  }
+  //avgdata =  $.map(avgdata, function(value, key) {return value;});
+
+  var sum = 0;
+  var len = 0;
+  for(var key in avgdata){
+    if(avgdata[key]>0){
+      sum+=avgdata[key];
+      len++;
+    }
+  }
+
+  var scalefactor = (sum/len)/idealavg;
+
+  document.getElementById('output').innerHTML = "A " + testmajor + " major at " + collegename + " would make $" + Math.round(scalefactor*majorearningdata[testmajor],2) + ".";
+
+}
+
+$(document).ready(function() {
+  $( function() {
+    var collegeList = loadFile("collegelist.txt").split("\n");
+    $( "#collegeSearch" ).autocomplete({
+      source: collegeList
+    }, "option", "position", { my: "center top", at: "center bottom"});
+  } );
+});
+
+$(document).ready(function () {
+  $('#collegeForm').on('submit', function(e) {
+      console.log("submitted")
+      e.preventDefault();
+      getIncome();
   });
+});
